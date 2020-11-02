2e6bbca41b9410d5396f257d47c7ffa9cd3eb0c4
Chris <lefatoum@hotmail.fr>
2020-11-03 00:36:33 +0100

    version 2

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
 # from data import Articles
-from flask_mysqldb import MySQLdb
+from flask_mysqldb import MySQL
+
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 from functools import wraps
@@ -9,10 +10,15 @@ app = Flask(__name__)
 
 # Config MySQL
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '123456'
+app.config['MYSQL_USER'] = 'root2'
+app.config['MYSQL_PASSWORD'] = 'root2'
 app.config['MYSQL_DB'] = 'myflaskapp'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+#app.config['MYSQL_HOST'] = 'localhost'
+#app.config['MYSQL_USER'] = 'root2'
+#app.config['MYSQL_PASSWORD'] = 'root2'
+#app.config['MYSQL_DB'] = 'myflaskapp'
+#app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 # init MYSQL
 mysql = MySQL(app)
 
